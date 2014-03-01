ca5380812fe77ad833fb54330f344f876280f983
meruriberu <meruriberu@51655719-9d22-e750-91e9-17dbf726e219>
2014-03-01 23:29:01 +0000

    Edit EventFormActivity - Category and All-day can now be selected

--- trunk/CSE3311_Calendar/src/com/example/cse3311_calendar/EventFormActivity.java
+++ trunk/CSE3311_Calendar/src/com/example/cse3311_calendar/EventFormActivity.java
@@ -1,20 +1,36 @@
 package com.example.cse3311_calendar;
 
+import com.example.cse3311_calendar.R.id;
+
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 public class EventFormActivity extends Activity {
 
 	Button mConfirm;
+	Spinner mCategorySpinner;
+	Spinner mAllDaySpinner;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_event_form);
+		
+		Spinner mCategorySpinner = (Spinner) findViewById(R.id.fCategory);
+		ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.category_array, android.R.layout.simple_spinner_item);
+		adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+		mCategorySpinner.setAdapter(adapter);
+		
+		Spinner mAllDaySpinner = (Spinner) findViewById(R.id.fAll_day);
+		adapter = ArrayAdapter.createFromResource(this, R.array.all_day_array, android.R.layout.simple_spinner_item);
+		adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+		mAllDaySpinner.setAdapter(adapter);
 		
 		mConfirm = (Button) this.findViewById(R.id.confirm);
 		mConfirm.setOnClickListener(new View.OnClickListener() {
