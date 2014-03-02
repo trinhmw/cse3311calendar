af9005937d81b49fa912dded2f4bd57bf4d3079d
meruriberu <meruriberu@51655719-9d22-e750-91e9-17dbf726e219>
2014-03-02 04:02:03 +0000

    -Edit EventFormActivity - Bundle and Intent made

--- trunk/CSE3311_Calendar/src/com/example/cse3311_calendar/EventFormActivity.java
+++ trunk/CSE3311_Calendar/src/com/example/cse3311_calendar/EventFormActivity.java
@@ -1,13 +1,13 @@
 package com.example.cse3311_calendar;
 
-import com.example.cse3311_calendar.R.id;
-
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.Toast;
 
@@ -16,18 +16,19 @@ public class EventFormActivity extends Activity {
 	Button mConfirm;
 	Spinner mCategorySpinner;
 	Spinner mAllDaySpinner;
+	EditText et;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_event_form);
 		
-		Spinner mCategorySpinner = (Spinner) findViewById(R.id.fCategory);
+		final Spinner mCategorySpinner = (Spinner) findViewById(R.id.fCategory);
 		ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.category_array, android.R.layout.simple_spinner_item);
 		adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 		mCategorySpinner.setAdapter(adapter);
 		
-		Spinner mAllDaySpinner = (Spinner) findViewById(R.id.fAll_day);
+		final Spinner mAllDaySpinner = (Spinner) findViewById(R.id.fAll_day);
 		adapter = ArrayAdapter.createFromResource(this, R.array.all_day_array, android.R.layout.simple_spinner_item);
 		adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 		mAllDaySpinner.setAdapter(adapter);
@@ -37,10 +38,57 @@ public class EventFormActivity extends Activity {
 			
 			@Override
 			public void onClick(View v) {
-				Toast.makeText(EventFormActivity.this, R.string.add_event, Toast.LENGTH_LONG).show();
+				String tag = (String) v.getTag();
+				Bundle bundle = new Bundle();
+				
+				et = (EditText) findViewById(R.id.fName);
+				String text = et.getText().toString();
+				//Toast.makeText(EventFormActivity.this, text, Toast.LENGTH_LONG).show();
+				
+				bundle.putString("name", text);
+				
+				et = (EditText) findViewById(R.id.fLocation);
+				text = et.getText().toString();
+				bundle.putString("location", text);
+				
+				et = (EditText) findViewById(R.id.fStart_date);
+				text = et.getText().toString();
+				bundle.putString("start_date", text);
+				
+				et = (EditText) findViewById(R.id.fEnd_date);
+				text = et.getText().toString();
+				bundle.putString("end_date", text);
+				
+				et = (EditText) findViewById(R.id.fStart_time);
+				text = et.getText().toString();
+				bundle.putString("start_time", text);
+				
+				et = (EditText) findViewById(R.id.fEnd_time);
+				text = et.getText().toString();
+				bundle.putString("end_time", text);
+				
+				et = (EditText) findViewById(R.id.fDescription);
+				text = et.getText().toString();
+				bundle.putString("description", text);
+				
+				
+				text = mCategorySpinner.getSelectedItem().toString();
+				bundle.putString("category", text);
+				
+				
+				text = mAllDaySpinner.getSelectedItem().toString();
+				bundle.putString("all_day", text);
+				
+				
+				Intent changeIntent = new Intent(EventFormActivity.this, MonthViewActivity.class);
+				changeIntent.putExtras(bundle);
+				//Toast.makeText(EventFormActivity.this, changeIntent.getExtras().getString("description"), Toast.LENGTH_LONG).show();
+				startActivity(changeIntent);
+				
 				
 			}
 		});
+		
 	}
 
 	@Override
@@ -48,6 +96,24 @@ public class EventFormActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.event_form, menu);
 		return true;
+	}
+	
+public void confirmClick(View view){
+		
+		String tag = (String) view.getTag();
+		
+		Bundle dataBundle = new Bundle();
+		
+		
+		et = (EditText) findViewById(R.id.fName);
+		String text = et.getText().toString();
+		
+		dataBundle.putString("name", text);	
+		
+		Intent changeIntent = new Intent(EventFormActivity.this, MonthViewActivity.class);
+		changeIntent.putExtras(dataBundle);
+		startActivity(changeIntent);
+		
 	}
 
 }
