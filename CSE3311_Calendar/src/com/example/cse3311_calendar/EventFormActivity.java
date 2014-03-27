0d7c6e4acabbba0065eb6a2893f364d86308571d
totallyunique11 <totallyunique11@51655719-9d22-e750-91e9-17dbf726e219>
2014-03-27 17:50:32 +0000

    Added support in day view for tapping an hour and getting an add event dialog for that hour

--- CSE3311_Calendar/src/com/example/cse3311_calendar/EventFormActivity.java
+++ CSE3311_Calendar/src/com/example/cse3311_calendar/EventFormActivity.java
@@ -1,5 +1,6 @@
 package com.example.cse3311_calendar;
 
+import java.util.Calendar;
 import java.util.Date;
 
 import android.app.Activity;
@@ -20,57 +21,97 @@ import android.widget.Toast;
  */
 public class EventFormActivity extends Activity {
 
-	Button mConfirm;
-	Spinner mCategorySpinner;
-	Spinner mAllDaySpinner;
-	EditText et;
+	private Button mConfirm;
+	private Spinner mCategorySpinner;
+	private Spinner mAllDaySpinner;
+	private EditText et;
+	private int startHour, endHour;
+	private int startMinute, endMinute;
+	private int day, month, year; 
 
-	
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_event_form);
-		
+
 		final Spinner mCategorySpinner = (Spinner) findViewById(R.id.fCategory);
 		ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.category_array, android.R.layout.simple_spinner_item);
 		adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 		mCategorySpinner.setAdapter(adapter);
-		
+
 		final Spinner mAllDaySpinner = (Spinner) findViewById(R.id.fAll_day);
 		adapter = ArrayAdapter.createFromResource(this, R.array.all_day_array, android.R.layout.simple_spinner_item);
 		adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 		mAllDaySpinner.setAdapter(adapter);
-		
+
 		final DatePicker mStartDate = (DatePicker) findViewById(R.id.fStart_date);
 		final DatePicker mEndDate = (DatePicker) findViewById(R.id.fEnd_date);
-		
+
 		final TimePicker mStartTime = (TimePicker) findViewById(R.id.fStart_time);
 		final TimePicker mEndTime = (TimePicker) findViewById(R.id.fEnd_time);
 		
+		// Get the message from the intent
+		Intent intent = getIntent();
+
+		Bundle inputBundle = intent.getExtras();
+
+		if (inputBundle != null) {
+			day = inputBundle.getInt("day");
+			month = inputBundle.getInt("month");
+			year = inputBundle.getInt("year");
+			startHour = inputBundle.getInt("hour");
+			startMinute = 0;
+			endMinute = 0;
+			
+		} 
+		//If intent is empty set current day
+		else {
+			Calendar cal = Calendar.getInstance();
+			day = cal.get(Calendar.DAY_OF_MONTH);
+			month = cal.get(Calendar.MONTH);
+			year = cal.get(Calendar.YEAR);
+			startHour = cal.get(Calendar.HOUR_OF_DAY);
+			startMinute = cal.get(Calendar.MINUTE);
+			endMinute  = cal.get(Calendar.MINUTE);
+		}
+		
+		endHour = startHour + 1;
+
+		mStartDate.updateDate(year, month, day);
+		mStartTime.setCurrentHour(startHour);
+		mStartTime.setCurrentMinute(startMinute);
+
+		mEndDate.updateDate(year, month, day);
+		mEndTime.setCurrentHour(endHour);
+		mEndTime.setCurrentMinute(endMinute);
+		
+		
+
 		mConfirm = (Button) this.findViewById(R.id.confirm);
 		mConfirm.setOnClickListener(new View.OnClickListener() {
-			
+
 			@Override
 			public void onClick(View v) {
-				
-				
+
+
 				String tag = (String) v.getTag();
 				//Bundle bundle = new Bundle();
-				
+
 				et = (EditText) findViewById(R.id.fName);
 				//String text = et.getText().toString();
 				String name = et.getText().toString();
 				//Toast.makeText(EventFormActivity.this, text, Toast.LENGTH_LONG).show();
-				
+
 				//bundle.putString("name", text);
-				
+
 				et = (EditText) findViewById(R.id.fLocation);
 				//text = et.getText().toString();
 				String location = et.getText().toString();
 				//bundle.putString("location", text);
-				
+
 				//Toast.makeText(EventFormActivity.this, "" + mStartDate.getYear(), Toast.LENGTH_LONG).show();
-				
+
 				Date startDate = new Date(mStartDate.getYear(), mStartDate.getMonth(), mStartDate.getDayOfMonth());
 				Date endDate = new Date(mEndDate.getYear(), mEndDate.getMonth(), mEndDate.getDayOfMonth());
 				int startTime = mStartTime.getCurrentHour().intValue();
@@ -79,31 +120,33 @@ public class EventFormActivity extends Activity {
 				int endTime = mEndTime.getCurrentHour().intValue();
 				endTime = endTime * 60;
 				endTime = endTime + mEndTime.getCurrentMinute().intValue();
+				
+				
 				/*
 				bundle.putInt("start_date_month", mStartDate.getMonth());
 				bundle.putInt("start_date_day", mStartDate.getDayOfMonth());
 				bundle.putInt("start_date_year", mStartDate.getYear());
-				
+
 				bundle.putInt("end_date_month", mEndDate.getMonth());
 				bundle.putInt("end_date_day", mEndDate.getDayOfMonth());
 				bundle.putInt("end_date_year", mEndDate.getYear());
-				
+
 				bundle.putInt("start_time_hour", mStartTime.getCurrentHour().intValue());
 				bundle.putInt("start_time_min", mStartTime.getCurrentMinute().intValue());
-				
+
 				bundle.putInt("end_time_hour", mEndTime.getCurrentHour().intValue());
 				bundle.putInt("end_time_min", mEndTime.getCurrentMinute().intValue());
-				*/
-				
+				 */
+
 				et = (EditText) findViewById(R.id.fDescription);
 				String description = et.getText().toString();
 				//text = et.getText().toString();
 				//bundle.putString("description", text);
-				
-				
+
+
 				int category = mCategorySpinner.getSelectedItemPosition();
 				//bundle.putString("category", text);
-				
+
 				boolean allDay;
 				String allDayString = mAllDaySpinner.getSelectedItem().toString();
 				if(allDayString.equals("Yes")){
@@ -112,10 +155,10 @@ public class EventFormActivity extends Activity {
 					allDay = false;
 				}
 				//bundle.putString("all_day", text);
-				
+
 				boolean result = AddEventController.addEvent(name, location, startDate, endDate, startTime, endTime, 
 						description, category, allDay);
-				
+
 				if(result == false){
 					Toast.makeText(EventFormActivity.this, "There was an error adding the Event", Toast.LENGTH_LONG).show();
 				}
@@ -128,17 +171,17 @@ public class EventFormActivity extends Activity {
 					changeIntent.putExtras(bundle);
 					startActivity(changeIntent);
 				}
-				
-				
+
+
 				//Toast.makeText(EventFormActivity.this, changeIntent.getExtras().getInt("start_time_hour"), Toast.LENGTH_LONG).show();
 				//String tmp = "" + changeIntent.getExtras().getInt("start_time_hour");
 				//Toast.makeText(EventFormActivity.this, tmp, Toast.LENGTH_LONG).show();
-				
-				
-				
+
+
+
 			}
 		});
-		
+
 	}
 
 	@Override
@@ -147,28 +190,28 @@ public class EventFormActivity extends Activity {
 		getMenuInflater().inflate(R.menu.event_form, menu);
 		return true;
 	}
-	
-/**
- * Handles when confirm button is clicked
- *
- * @param view the view
- */
-public void confirmClick(View view){
-		
+
+	/**
+	 * Handles when confirm button is clicked
+	 *
+	 * @param view the view
+	 */
+	public void confirmClick(View view){
+
 		String tag = (String) view.getTag();
-		
+
 		Bundle dataBundle = new Bundle();
-		
-		
+
+
 		et = (EditText) findViewById(R.id.fName);
 		String text = et.getText().toString();
-		
+
 		dataBundle.putString("name", text);	
-		
+
 		Intent changeIntent = new Intent(EventFormActivity.this, MonthViewActivity.class);
 		changeIntent.putExtras(dataBundle);
 		startActivity(changeIntent);
-		
+
 	}
 
 }
